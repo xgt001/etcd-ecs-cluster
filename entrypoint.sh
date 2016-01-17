38396146a927108a00ff48a06c6fabd9bedc8078
Ganesh Hegde <xgt008@gmail.com>
2016-01-17 10:20:37 +0530

    setup private cluster if ETCD_TOKEN is not given

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,26 @@
 
 IP_V4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4);
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id);
+PUBLIC_V4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4);
+
+#Run instructions:
+#     docker run -d -e ETCD_TOKEN=<your token> <docker image name>"
+# More instructions
+# https://coreos.com/os/docs/latest/cluster-discovery.html"
+#If ETCD_TOKEN is not specified we run it as a private discovery cluster
 
 if [ -z $ETCD_TOKEN ]
  then
-  echo "you need to define variable ETCD_TOKEN"
-  echo ""
-  echo "    docker run -d -e ETCD_TOKEN=<your token> <docker image name>"
-  echo ""
-  echo "you can find manual at"
-  echo "https://coreos.com/os/docs/latest/cluster-discovery.html"
-  exit 1
+  cd /opt/etcd
+  ./etcd \
+    -name etcd \
+    -advertise-client-urls http://$PUBLIC_V4:2379,http://$PUBLIC_V4:4001 \
+    -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+    -initial-advertise-peer-urls http://$PUBLIC_V4:2380 \
+    -listen-peer-urls http://0.0.0.0:2380 \
+    -initial-cluster etcd0=http://$PUBLIC_V4:2380 \
+    -initial-cluster-token etcd-cluster-1 \
+    -initial-cluster-state new
 else
  cd /opt/etcd
  ./etcd \
